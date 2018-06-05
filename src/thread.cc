2618fb4972c346f24b8164292cd52a25789ae2d9
cincat <cincat>
2018-06-05 16:38:19 +0800

    add thread facility to asnet

--- src/thread.cc
+++ src/thread.cc
@@ -1,10 +1,32 @@
+#include <errno.h>
 
 #include <thread.h>
 #include <log.h>
 
 namespace asnet {
-    Thread::Thread() {
+    void *startThread(void *args) {
+        Thread *t = static_cast<Thread*>(args);
+        t->threadFunc();
+        return nullptr;
+    }
+    Thread::Thread() :loop_(), mutex_(), cond_(mutex_){
         int err = 0;
-        // err = pthread_create(&thread_, nullptr, )
+        err = pthread_create(&thread_, nullptr, startThread, this);
+        if (err < 0) {
+            LOG_ERROR << strerror(errno) << "\n";
+        }
+    }
+
+    void Thread::threadFunc() {
+        cond_.wait();
+        loop_.run();
+    }
+
+    void Thread::run() {
+        cond_.notify();
+    }
+
+    EventLoop *Thread::getEventLoop() {
+        return &loop_;
     }
 }
--- src/thread.h
+++ src/thread.h
@@ -3,6 +3,8 @@
 
 #include <event_loop.h>
 #include <pthread.h>
+#include <mutex.h>
+#include <condition.h>
 
 namespace asnet {
 
@@ -14,9 +16,14 @@ public:
 
     Thread(Thread &) = delete;
     Thread& operator =(Thread &) = delete;
+    
+    void run();
+    void threadFunc();
 private:
     EventLoop loop_;
     pthread_t thread_;
+    Mutex mutex_;
+    Condition cond_;
 };
 
 }// end of namespace asnet
